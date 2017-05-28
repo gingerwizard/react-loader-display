46132ddc547ce0ee07a242141f1bfafe7f5fa6d5
albert <albertusvdmerwe@gmail.com>
2017-05-28 13:37:57 +0200

    Extracted styling to seperate module

---
+++ Styling/Styles.js
@@ -0,0 +1,46 @@
+var StylesObj={
+    BackdropVisible:{
+        display:'block',
+        position:'fixed',
+        top:0,
+        left:0,
+        right:0,
+        bottom:0,
+        width:'100%',
+        height:'100vh',
+        textAlign:'center'
+    },
+    LoaderHidden:{
+        display:'none'
+    },
+    Modal:{
+        display:'block',
+        marginLeft:'auto',
+        marginRight:'auto',
+        width:300,
+        borderRadius:'5px',
+        marginTop:150,
+        paddingTop:30,
+        paddingBottom:30,
+        textAlign:'center'
+    },
+    LoadingImage:{
+        display:'block',
+        width:50,
+        marginLeft:'auto',
+        marginRight:'auto',
+        verticalAlign:'middle',
+        marginBottom:30
+    },
+    LoadingText:{
+        display:'block',
+        width:'90%',
+        marginLeft:'auto',
+        marginRight:'auto',
+        verticalAlign:'middle',
+        marginTop:30,
+        textAlign:'center'
+    }
+};
+
+module.exports=StylesObj;
--- react-loader-display.js
+++ react-loader-display.js
@@ -2,6 +2,8 @@
 var createReactClass = require('create-react-class');
 var h = require('react-hyperscript');
 var PropTypes=require('prop-types');
+var $=require('jquery');
+var Styles=require('./Styling/Styles');
 
 var reactLoaderDisplay = createReactClass({
     displayName: 'ReactLoaderDisplay',
@@ -25,70 +27,24 @@ var reactLoaderDisplay = createReactClass({
         TextColor: PropTypes.string
     },
     render: function() {
-        var LoadingContainerStyle=this.props.IsLoading?{
-            display:'block',
-            position:'fixed',
-            top:0,
-            left:0,
-            right:0,
-            bottom:0,
-            width:'100%',
-            height:'100vh',
-            backgroundColor:this.props.BackDropRGBA,
-            zIndex:this.props.ZIndex,
-            textAlign:'center'
-        }:{
-            display:'none'
-        };
 
-        var LoaderChildComponentStyle={
-            display:'block',
-            marginLeft:'auto',
-            marginRight:'auto',
-            width:300,
-            borderRadius:'5px',
-            marginTop:150,
-            backgroundColor:this.props.ForeGroundColor,
-            paddingTop:30,
-            paddingBottom:30,
-            textAlign:'center'
-        };
+        var Style=$.extend({},Styles);
+        Style.BackdropVisible.backgroundColor=this.props.BackDropRGBA;
+        Style.BackdropVisible.zIndex=this.props.ZIndex;
+        Style.Modal.backgroundColor=this.props.ForeGroundColor;
+        Style.LoadingText.color=this.props.TextColor;
 
-        var LoadingDisplayImage={
-            display:'block',
-            width:50,
-            marginLeft:'auto',
-            marginRight:'auto',
-            verticalAlign:'middle',
-            marginBottom:30
-        };
-
-        var LoadingDisplayText={
-            display:'block',
-            width:'90%',
-            marginLeft:'auto',
-            marginRight:'auto',
-            verticalAlign:'middle',
-            marginTop:30,
-            color:this.props.TextColor,
-            textAlign:'center'
-        };
-
-        if(this.props.IsLoading===false)
-        {
-            return null;
-        }
-
-    return (
-        h('div',{style:LoadingContainerStyle},[
-                h('div',{style:LoaderChildComponentStyle},[
-                    h('img',{src:this.props.LoadingImage,style:LoadingDisplayImage}),
-                    h('div',{style:LoadingDisplayText},this.props.LoaderMessage)
+        var ComponentObj=(
+            h('div',{style:
+                this.props.IsLoading?Style.BackdropVisible:Style.LoaderHidden},[
+                h('div',{style:Style.Modal},[
+                    h('img',{src:this.props.LoadingImage,style:Style.LoadingImage}),
+                    h('div',{style:Style.LoadingText},this.props.LoaderMessage)
                 ])
             ])
-    )
+        );
 
-
+        return this.props.IsLoading?ComponentObj:null;
     }
 });
 
