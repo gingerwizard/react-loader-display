a6e45c03dc76980d67304cb118adddb70b2dda5a
albertusvdmerwe <albertusvdmerwe@gmail.com>
2017-06-07 16:27:44 +0200

    TypeScript is used to transpile jsx and js

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_loader_display_1 = require("./react-loader-display");
-exports.loader = react_loader_display_1.default;
-//# sourceMappingURL=app.js.map
